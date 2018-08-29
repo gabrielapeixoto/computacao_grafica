34b201d47ee2667bc356ded05a1f1a8f44fa585b
Henrique de Almeida <henriqueribeirodealmeida@MacBook-Pro-de-Henrique.local>
2018-08-29 11:21:57 -0300

    formata código

--- polygon.c
+++ polygon.c
@@ -10,80 +10,76 @@
 
 GLenum doubleBuffer;   
  
-struct polygon 
-{
+struct polygon{
     float v[3];
 } pvertex[MAXVERTEXS];
 
-int windW, windH;
-int cc;
+int windowWidth, windowHeight;
+int menuItem;
 int tipoPoligono;
-int ss = -1;
+int vertice = -1; // se negativo, nenhum vértice selecionado
 int numPontos = 0;
 
-void circulo(float r, float ang, float pp[3])
-{
+void circulo(float r, float ang, float pp[3]){
 	pp[0] = (float)(r * cos(ang));
 	pp[1] = (float)(r * sin(ang));
 	pp[2] = (float)0.0;
 }
 
 
-int clipVertex(int x, int y)
-{
+int clipVertex(int x, int y){
 	int i;
 	float d;
-	ss=-1;   printf("\n clipVertex (%d, %d) ", x, y); 
-	for (i=0; i<NPOLYGON; i++) {
+	
+ 	vertice = -1;   
+
+	printf("\n clipVertex (%d, %d) ", x, y); 
+
+	for (i=0; i<NPOLYGON; i++){
 		d = sqrt(pow((pvertex[i].v[0]-x), 2.0) + pow((pvertex[i].v[1]-y), 2.0));
+		
 		printf(" i: %i -> d:%4.2f;  ", i, d);
 		if(d < 3.0){
-			ss = i;
+			vertice = i;
 			break;
 		}
 	}
-	return ss;
+	return vertice;
 }
 
-void init(void)
-{
+void init(void){
 	int i;
 	float ang;
-	cc=1;
+	menuItem = 1;
 	tipoPoligono = GL_LINE;
 
-	for(i=0; i<NPOLYGON; i++)
-	{
+	for(i = 0; i < NPOLYGON; i++){
 		ang = (float)(i) * (2.0 * PHI)/((float)(NPOLYGON));
 		circulo(70.0, ang, pvertex[i].v);
 	}
 }
 
-static void Reshape(int width, int height)
-{
-    windW = width/2; 
-    windH = height/2;
+static void Reshape(int width, int height){
+    windowWidth = width/2; 
+    windowHeight = height/2;
 
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 //    gluPerspective(60.0, 1.0, 0.1, 1000.0);
-    gluOrtho2D(-windW, windW, -windH, windH);
+    gluOrtho2D(-windowWidth, windowWidth, -windowHeight, windowHeight);
 
     glMatrixMode(GL_MODELVIEW);
 }
 
-static void Key(unsigned char key, int x, int y)
-{
-    switch (key) 
-	{
+static void Key(unsigned char key, int x, int y){
+    switch (key) {
       case 27:
-			exit(0);
+		exit(0);
     }
 }
 
-void coord_line(void)
-{
+void coord_line(void){
     glLineWidth(1);
 
 	glColor3f(1.0, 0.0, 0.0);
@@ -91,20 +87,19 @@ void coord_line(void)
 	// vertical line
 
 	glBegin(GL_LINE_STRIP);
-		glVertex2f(-windW, 0);
-		glVertex2f(windW, 0);
+	glVertex2f(-windowWidth, 0);
+	glVertex2f(windowWidth, 0);
     glEnd();
 
 	// horizontal line 
 
     glBegin(GL_LINE_STRIP);
-		glVertex2f(0, -windH);
-		glVertex2f(0, windH);
+	glVertex2f(0, -windowHeight);
+	glVertex2f(0, windowHeight);
     glEnd();
 }
 
-void PolygonDraw(void)
-{
+void PolygonDraw(void){
 	int i;
 
 	glColor3f(0.0, 0.0, 0.0); 
@@ -112,36 +107,31 @@ void PolygonDraw(void)
 	glPolygonMode(GL_FRONT_AND_BACK, tipoPoligono);
 
 	glBegin(GL_POLYGON);
-	for(i=0; i<NPOLYGON; i+=cc)
-	{
+	for(i = 0; i < NPOLYGON; i += menuItem){
 		glVertex2fv(pvertex[i].v);
 	}
 	glEnd();
 }
 
-void PointDraw (void)
-{ 
+void PointDraw (void){ 
     int i;
     glColor3f(0.0, 0.0, 1.0);
 	glPointSize(3);
 	glBegin(GL_POINTS); 				
-	for(i=0; i<NPOLYGON; i+=cc)
-	{
+	for(i = 0; i < NPOLYGON; i += menuItem){
 		glVertex2fv(pvertex[i].v);
 	} 
 	glEnd();  
     
-    if (ss>=0)
-    {
+    if  (vertice >= 0){
         glColor3f(1.0, 0.0, 0.0);
 	    glBegin(GL_POINTS); 
-           glVertex2fv(pvertex[ss].v);
+        glVertex2fv(pvertex vertice].v);
         glEnd();        
     }        
 }
 
-static void Draw(void)
-{
+static void Draw(void){
     glClearColor(1.0, 1.0, 1.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT);
 
@@ -151,18 +141,15 @@ static void Draw(void)
 	
 	PointDraw();
 
-    if (doubleBuffer) 
-	{
+    if (doubleBuffer) {
 	   glutSwapBuffers(); 
     } else {
 	   glFlush();     
     }
 }
 
-void processMenuEvents(int option) 
-{
-	switch (option) 
-	{
+void proc verticeMenuEvents(int option){
+	switch (option) {
 		case 4 : 
 			if (tipoPoligono == GL_LINE)
 				tipoPoligono = GL_FILL;
@@ -173,75 +160,67 @@ void processMenuEvents(int option)
 	glutPostRedisplay();
 }
 
-void processSubMenuEvents(int option) 
-{
-	cc = option;
+void proc verticeSubMenuEvents(int option){
+	menuItem = option;
 	glutPostRedisplay();
 }
 
 
-static void Args(int argc, char **argv)
-{
+static void Args(int argc, char **argv){
     GLint i;
 
     doubleBuffer = GL_FALSE;
 
-    for (i = 1; i < argc; i++) 
-	{
-	   if (strcmp(argv[i], "-sb") == 0)   
-	   {
+    for (i = 1; i < argc; i++) {
+	   if (strcmp(argv[i], "-sb") == 0){
 	      doubleBuffer = GL_FALSE;
-	   } else if (strcmp(argv[i], "-db") == 0) 
-	   {
+	   } else if (strcmp(argv[i], "-db") == 0){
 	      doubleBuffer = GL_TRUE;
 	   }
     }
 }
 
-void createGLUTMenus() 
-{
+void createGLUTMenus(){
 	int menu,submenu;
 
-	submenu = glutCreateMenu(processSubMenuEvents);
+	submenu = glutCreateMenu(proc verticeSubMenuEvents);
 	glutAddMenuEntry("Polígono",1);
 	glutAddMenuEntry("Triângulo",2);
 	glutAddMenuEntry("Retângulo",3);
 
-	menu = glutCreateMenu(processMenuEvents);
+	menu = glutCreateMenu(proc verticeMenuEvents);
 	glutAddMenuEntry("Linha/Prenchido",4);
 	glutAddSubMenu("Tipo Poligono",submenu);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
 
-void motionMouse(int x, int y)
-{
+void motionMouse(int x, int y){
 	glColor3f(0.0, 1.0, 0.0);
 	glPointSize(3);
 	glBegin(GL_POINTS); 				
-		x = x - windW;     y = windH - y;
-		glVertex2i(x, y);  
+	x = x - windowWidth;     
+	y = windowHeight - y;
+	glVertex2i(x, y);  
 	glEnd(); 
 }
 
-void mouseClick(int button, int state, int x, int y)
-{ 
+void mouseClick(int button, int state, int x, int y){ 
 	if(button == GLUT_LEFT_BUTTON)
-		if(state == GLUT_DOWN)
-		{
-			x = x - windW;    y = windH - y;
-			ss = clipVertex(x, y);
-			if(ss>-1) {
+		if(state == GLUT_DOWN){
+			x = x - windowWidth;    
+			y = windowHeight - y;
+		 vertice = clipVertex(x, y);
+			if (vertice > -1) {
 				glColor3f(1.0, 0.0, 0.0);
 				glPointSize(3);
 				glBegin(GL_POINTS); 
-					glVertex2i(x, y);
+				glVertex2i(x, y);
 				glEnd();
 			}
 		}
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     GLenum type;
 
     glutInit(&argc, argv);
