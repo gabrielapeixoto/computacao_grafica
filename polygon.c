48ffa6f2f3fdbe73254af34fba5e84670fd250b2
gabrielapeixoto <peixoto.sgabriela@gmail.com>
2018-09-04 00:15:34 -0300

    movimentação do vértice

--- polygon.c
+++ polygon.c
@@ -10,14 +10,14 @@
 
 GLenum doubleBuffer;   
  
-struct polygon{
+struct polygon {
     float v[3];
 } pvertex[MAXVERTEXS];
 
-int windowWidth, windowHeight;
+int windW, windH;
 int menuItem;
 int tipoPoligono;
-int vertice = -1; // se negativo, nenhum vértice selecionado
+int vertice = -1;
 int numPontos = 0;
 
 void circulo(float r, float ang, float pp[3]){
@@ -30,17 +30,14 @@ void circulo(float r, float ang, float pp[3]){
 int clipVertex(int x, int y){
 	int i;
 	float d;
-	
- 	vertice = -1;   
+	vertice = -1;
 
-	printf("\n clipVertex (%d, %d) ", x, y); 
-
-	for (i=0; i<NPOLYGON; i++){
+	for (i = 0; i < NPOLYGON; i++) {
 		d = sqrt(pow((pvertex[i].v[0]-x), 2.0) + pow((pvertex[i].v[1]-y), 2.0));
-		
-		printf(" i: %i -> d:%4.2f;  ", i, d);
 		if(d < 3.0){
 			vertice = i;
+			printf("\n clipVertex (%d, %d) ", x, y);
+			printf(" i: %i -> d:%4.2f;  ", i, d);
 			break;
 		}
 	}
@@ -60,22 +57,23 @@ void init(void){
 }
 
 static void Reshape(int width, int height){
-    windowWidth = width/2; 
-    windowHeight = height/2;
+    windW = width/2; 
+    windH = height/2;
 
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 //    gluPerspective(60.0, 1.0, 0.1, 1000.0);
-    gluOrtho2D(-windowWidth, windowWidth, -windowHeight, windowHeight);
+    gluOrtho2D(-windW, windW, -windH, windH);
 
     glMatrixMode(GL_MODELVIEW);
 }
 
 static void Key(unsigned char key, int x, int y){
-    switch (key) {
+    switch (key) 
+	{
       case 27:
-		exit(0);
+			exit(0);
     }
 }
 
@@ -87,15 +85,15 @@ void coord_line(void){
 	// vertical line
 
 	glBegin(GL_LINE_STRIP);
-	glVertex2f(-windowWidth, 0);
-	glVertex2f(windowWidth, 0);
+		glVertex2f(-windW, 0);
+		glVertex2f(windW, 0);
     glEnd();
 
 	// horizontal line 
 
     glBegin(GL_LINE_STRIP);
-	glVertex2f(0, -windowHeight);
-	glVertex2f(0, windowHeight);
+		glVertex2f(0, -windH);
+		glVertex2f(0, windH);
     glEnd();
 }
 
@@ -117,16 +115,17 @@ void PointDraw (void){
     int i;
     glColor3f(0.0, 0.0, 1.0);
 	glPointSize(3);
-	glBegin(GL_POINTS); 				
+	glBegin(GL_POINTS);
+
 	for(i = 0; i < NPOLYGON; i += menuItem){
 		glVertex2fv(pvertex[i].v);
 	} 
 	glEnd();  
     
-    if  (vertice >= 0){
+    if (vertice >= 0){
         glColor3f(1.0, 0.0, 0.0);
 	    glBegin(GL_POINTS); 
-        glVertex2fv(pvertex vertice].v);
+           glVertex2fv(pvertex[vertice].v);
         glEnd();        
     }        
 }
@@ -141,6 +140,8 @@ static void Draw(void){
 	
 	PointDraw();
 
+	glutPostRedisplay();
+
     if (doubleBuffer) {
 	   glutSwapBuffers(); 
     } else {
@@ -148,7 +149,7 @@ static void Draw(void){
     }
 }
 
-void proc verticeMenuEvents(int option){
+void proceverticeMenuEvents(int option) {
 	switch (option) {
 		case 4 : 
 			if (tipoPoligono == GL_LINE)
@@ -160,7 +161,7 @@ void proc verticeMenuEvents(int option){
 	glutPostRedisplay();
 }
 
-void proc verticeSubMenuEvents(int option){
+void proceverticeSubMenuEvents(int option) {
 	menuItem = option;
 	glutPostRedisplay();
 }
@@ -172,23 +173,23 @@ static void Args(int argc, char **argv){
     doubleBuffer = GL_FALSE;
 
     for (i = 1; i < argc; i++) {
-	   if (strcmp(argv[i], "-sb") == 0){
+	   if (strcmp(argv[i], "-sb") == 0)   {
 	      doubleBuffer = GL_FALSE;
-	   } else if (strcmp(argv[i], "-db") == 0){
+	   } else if (strcmp(argv[i], "-db") == 0) {
 	      doubleBuffer = GL_TRUE;
 	   }
     }
 }
 
-void createGLUTMenus(){
+void createGLUTMenus() {
 	int menu,submenu;
 
-	submenu = glutCreateMenu(proc verticeSubMenuEvents);
+	submenu = glutCreateMenu(proceverticeSubMenuEvents);
 	glutAddMenuEntry("Polígono",1);
 	glutAddMenuEntry("Triângulo",2);
 	glutAddMenuEntry("Retângulo",3);
 
-	menu = glutCreateMenu(proc verticeMenuEvents);
+	menu = glutCreateMenu(proceverticeMenuEvents);
 	glutAddMenuEntry("Linha/Prenchido",4);
 	glutAddSubMenu("Tipo Poligono",submenu);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
@@ -198,23 +199,26 @@ void motionMouse(int x, int y){
 	glColor3f(0.0, 1.0, 0.0);
 	glPointSize(3);
 	glBegin(GL_POINTS); 				
-	x = x - windowWidth;     
-	y = windowHeight - y;
-	glVertex2i(x, y);  
+		x = x - windW;
+		y = windH - y;
+		pvertex[vertice].v[0] = x;
+		pvertex[vertice].v[1] = y;
+		Draw();
+		glVertex2i(x, y);
 	glEnd(); 
 }
 
 void mouseClick(int button, int state, int x, int y){ 
 	if(button == GLUT_LEFT_BUTTON)
 		if(state == GLUT_DOWN){
-			x = x - windowWidth;    
-			y = windowHeight - y;
-		 vertice = clipVertex(x, y);
-			if (vertice > -1) {
+			x = x - windW;
+			y = windH - y;
+			vertice = clipVertex(x, y);
+			if(vertice>-1) {
 				glColor3f(1.0, 0.0, 0.0);
 				glPointSize(3);
 				glBegin(GL_POINTS); 
-				glVertex2i(x, y);
+					glVertex2i(x, y);
 				glEnd();
 			}
 		}
